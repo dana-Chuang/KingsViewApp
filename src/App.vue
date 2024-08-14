21df9a1a7a32b87f15e6884f2ef50884634419e7
Chen Ling Chuang <cchuang2@usc.edu>
2024-08-15 01:03:06 +0800

    chore: adjust layout

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div class="app">
+  <div class="contentView">
     <div class="navigation-bar">
       <router-link to="/user">Users</router-link> |
       <router-link to="/admin">Admin</router-link>
@@ -13,7 +13,7 @@ import { RouterLink, RouterView } from 'vue-router'
 </template>
 
 <style scoped>
-.app {
+.contentView {
   width: 100%;
   font-family: Avenir, Arial, Helvetica, sans-serif;
   color: #2c3e50;
@@ -25,30 +25,5 @@ import { RouterLink, RouterView } from 'vue-router'
 .navigation-bar {
   font-size: 25px;
   text-align: center;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  .app {
-    display: flex;
-    align-items: flex-start;
-  }
 }
 </style>
