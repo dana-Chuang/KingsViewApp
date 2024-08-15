32bd56b38ed82a77269f479304cb03e298aefe53
Chen Ling Chuang <cchuang2@usc.edu>
2024-08-15 14:01:05 -0700

    feat(top-bar): add top bar and logo

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+//    <img class="logo" src="../src/components/icons/logo.png" />
 </script>
 
 <template>
+  <div class="top-bar">
+    <img class="logo" src="../src/components/icons/logo.png" />
+  </div>
   <div class="contentView">
     <div class="navigation-bar">
       <router-link to="/user">Users</router-link> |
@@ -13,8 +17,27 @@ import { RouterLink, RouterView } from 'vue-router'
 </template>
 
 <style scoped>
+.top-bar {
+  display: flex;
+  position: sticky;
+  background: black;
+  padding: 1.25em;
+  width: auto;
+  height: 76px;
+  justify-content: center;
+}
+
+.logo {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 168px;
+  height: auto;
+}
+
 .contentView {
   width: 100%;
+  padding-top: 15px;
   font-family: Avenir, Arial, Helvetica, sans-serif;
   color: #2c3e50;
   display: flex;
@@ -26,5 +49,18 @@ import { RouterLink, RouterView } from 'vue-router'
 .navigation-bar {
   font-size: 25px;
   text-align: center;
+}
+
+a {
+  text-decoration: none;
+  color: #c41320;
+  transition: 0.4s;
+  padding: 3px;
+}
+
+@media (hover: hover) {
+  a:hover {
+    background-color: #d8d8d8;
+  }
 }
 </style>
